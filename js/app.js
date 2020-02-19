0f29d965c77261cd2ed51d185cc8f5e5e15a54c3
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-02-19 12:36:31 +0100

    feat: front page done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const dateElement = document.getElementById('date');
+
+// Shows today's date
+
+const options = {
+  weekday: "long",
+  month: "short",
+  day: "numeric",
+  year: "numeric"
+}
+
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-us", options);
