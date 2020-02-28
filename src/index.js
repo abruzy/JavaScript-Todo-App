aaaf8a817435ec592cc12db4ef5f69fb21778996
collins <ugwucollins64@gmail.com>
2020-02-28 17:30:25 +0100

    dynamically display projects when creating tasks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import { addTodo, onPageLoad } from './tasks';
 import addProject from './project';
+import ProjectStorage from './projectstorage';
 
 document.getElementById('task-button').addEventListener('click',
   () => {
     document.querySelector('.task-modal').style.display = 'flex';
+    const select = document.createElement('SELECT');
+    select.setAttribute('class', 'custom-select')
+    const ps = ProjectStorage().getProject();
+    ps.forEach((element, index) => {
+      const option = document.createElement('OPTION');
+      option.setAttribute('value', `${index}`);
+      option.innerHTML = `${element.name}`;
+      select.appendChild(option);
+    });
+
+    const selectDiv = document.querySelector('.projects-select')
+    selectDiv.appendChild(select);
   });
 
-// document.querySelector('.close').addEventListener('click',
-//   () => {
-//     document.querySelector('.bg-modal').style.display = 'none';
-//   });
+document.querySelector('.close').addEventListener('click',
+  () => {
+    document.querySelector('.bg-modal').style.display = 'none';
+  });
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   document.getElementById('add').addEventListener('click', addTodo);
@@ -23,7 +36,7 @@ document.querySelector('.add-project').addEventListener('click', () => {
 
 const refreshPage = () => {
   window.location.reload();
-} 
+}
 
 document.querySelector('.project-button').addEventListener('click', () => {
   const name = document.querySelector('#project-name').value;
